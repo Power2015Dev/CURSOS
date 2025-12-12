5521ba3238b8bd319ef657959543533e06375a9c
Power2015Dev <antonio23102005@gmail.com>
2025-12-11 21:28:17 -0400

    continuacion de courses

--- modulos/configuracion/js/courses.js
+++ modulos/configuracion/js/courses.js
@@ -1,29 +1,42 @@
 import { MyCourses } from '../../../constructores/Courses_class.js';
+import { listaCursos } from './fakeDB.js';
+listaCursos.forEach(element => {agregar_tarjetas(element.Titulo, element.Completacion, element.Imagen);});
+
 
 // Agregando en mis cursos (temporal)
-const agregar_temp = document.getElementById("agregartemp");
-agregar_temp.addEventListener("click", function(){
-    const test2 = new MyCourses("Programación orientada a objetos", 100);
+// const agregar_temp = document.getElementById("agregartemp");
+// agregar_temp.addEventListener("click", function(){
+//     const test2 = new MyCourses("Programación orientada a objetos", 50, "../../imagenes/dashboard_img/Programacion.png");
+//     const message = document.getElementById("Courses_available");
+//     if(message){
+//         message.remove();
+//     }
+//     test2.add_courses();
+// });
+
+// Eliminando en mis cursos (temporal)
+// const eliminar_temp = document.getElementById("eliminartemp");
+// eliminar_temp.addEventListener("click", function(){
+//     const test2 = new MyCourses();
+//     const fatherbox = document.querySelector(".cursos_actuales");
+//     const existing_message = document.getElementById("Courses_available");
+//     test2.remove_courses();
+//     if(fatherbox.children.length == 0 && !existing_message){
+//         const message = document.createElement("p");
+//         message.textContent = "No hay cursos guardados";
+//         message.id = "Courses_available";
+//         message.classList.add("course_style");
+//         fatherbox.appendChild(message);
+
+//     }
+// });
+
+function agregar_tarjetas(Titulo, Completacion, Imagen){
+    const Course_Card = new MyCourses(Titulo, Completacion, Imagen);
     const message = document.getElementById("Courses_available");
     if(message){
         message.remove();
     }
-    test2.add_courses();
-    console.log("testing");
-});
-// Eliminando en mis cursos (temporal)
-const eliminar_temp = document.getElementById("eliminartemp");
-eliminar_temp.addEventListener("click", function(){
-    const test2 = new MyCourses("Programación orientada a objetos", 100);
-    const fatherbox = document.querySelector(".cursos_actuales");
-    const existing_message = document.getElementById("Courses_available");
-    test2.remove_courses();
-    if(fatherbox.children.length == 0 && !existing_message){
-        const message = document.createElement("p");
-        message.textContent = "No hay cursos guardados";
-        message.id = "Courses_available";
-        message.classList.add("course_style");
-        fatherbox.appendChild(message);
+    Course_Card.add_courses();
+}
 
-    }
-});
---
+++ modulos/configuracion/js/fakeDB.js
@@ -0,0 +1,23 @@
+export const listaCursos = [
+    {
+        id: 1,
+        Titulo: "I HAD SEX",
+        Imagen: "https://i.redd.it/origin-of-the-dawko-i-had-sex-video-v0-53jt18zhi4we1.jpg?width=640&format=pjpg&auto=webp&s=601f03377f42f4e2014fa045bcc9ec602b517c28", 
+        Completacion: 45,
+        Estado: "En progreso"
+    },
+    {
+        id: 2,
+        Titulo: "Minecraft hardcore but I have carbon monoxide poisoning",
+        Imagen: "https://i.ytimg.com/vi/8Wow0tjOvxg/maxresdefault.jpg",
+        Completacion: 80,
+        Estado: "En progreso"
+    },
+    {
+        id: 3,
+        Titulo: "Porque dice la verdad?",
+        Imagen: "https://media.tenor.com/MkI7jDQ5yboAAAAe/youtube-thumbnail.png",
+        Completacion: 100,
+        Estado: "Finalizado"
+    }
+];
