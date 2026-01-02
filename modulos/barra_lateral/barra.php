91f9fca60e24ac040f70f14d3ab8502903911852
jorluis34 <luisordonezordonez439@gmail.com>
2026-01-02 15:32:48 -0400

    lista la barra lateral con el php

---
+++ modulos/barra_lateral/barra.php
@@ -0,0 +1,38 @@
+<?php
+    // Detecta el nombre del archivo actual (ej: ingresos.php)
+    $pagina_actual = basename($_SERVER['PHP_SELF']);
+?>
+<link rel="stylesheet" href="../../modulos/barra.css">
+<aside class="lateral">
+    <ul class="menu">
+        
+        <a href="/CURSOS/modulos/vendedor/dashboard_seller.php">
+            <li class="opcion <?php if($pagina_actual == 'resumen.php'){ echo 'activo'; } ?>">
+                Resumen
+            </li>
+        </a>
+
+        <a href="/CURSOS/modulos/vendedor/gestioncurso.php">
+            <li class="opcion <?php if($pagina_actual == 'cursos.php'){ echo 'activo'; } ?>">
+                Gestionar Cursos
+            </li>
+        </a>
+
+        <a href="/CURSOS/modulos/vendedor/ingresos.php">
+            <li class="opcion <?php if($pagina_actual == 'ingresos.php'){ echo 'activo'; } ?>">
+                Ingresos
+            </li>
+        </a>
+
+        <a href="configuracion.php">
+            <li class="opcion <?php if($pagina_actual == 'configuracion.php'){ echo 'activo'; } ?>">
+                Configuración
+            </li>
+        </a>
+
+    </ul>
+
+    <div class="inferior">
+        <button class="boton">+ Nuevo Curso</button>
+    </div>
+</aside>
